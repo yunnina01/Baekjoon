0d52ed0158dd4c1ae3dda239f37ac23e4ceb3d61
TaeHyun Yun <100751725+yunnina01@users.noreply.github.com>
2022-10-09 14:11:47 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.c
+++ 백준/Bronze/2588. 곱셈/곱셈.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    int num1, num2;
-    scanf("%d %d", &num1, &num2);
-    printf("%d\n", num1 * (num2 % 10));
-    printf("%d\n", num1 * (num2 % 100 / 10));
-    printf("%d\n", num1 * (num2 / 100));
-    printf("%d\n", num1 * num2);
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a * (b % 10));
+    printf("%d\n", a * (b % 100 / 10));
+    printf("%d\n", a * (b / 100));
+    printf("%d\n", a * b);
 
     return 0;
 }
