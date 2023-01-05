3aaafa3800d73d24f3c36349100e37a60f612b38
TaeHyun Yun <100751725+yunnina01@users.noreply.github.com>
2023-01-05 10:15:44 +0900

    [Silver III] Title: N과 M (1), Time: 36 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void back(int *arr, int cnt, int N, int M){
+    int i, j, check;
+    if(cnt == M){
+        for(i = 0; i < M; i++)
+            printf("%d ", arr[i]);
+        puts("");
+    }
+    else{
+        for(i = 1; i <= N; i++){
+            check = 0;
+            for(j = 0; j < cnt; j++){
+                if(arr[j] == i)
+                    check = 1;
+            }
+            if(!check){
+                arr[cnt] = i;
+                back(arr, cnt + 1, N, M);
+            }
+        }
+    }
+}
+
+int main(){
+    int N, M;
+    scanf("%d %d", &N, &M);
+    int arr[N];
+    back(arr, 0, N, M);
+
+    return 0;
+}
