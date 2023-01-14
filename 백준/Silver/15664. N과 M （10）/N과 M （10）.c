fb20d13624335b6bb6213a329715fc13925853bb
TaeHyun Yun <100751725+yunnina01@users.noreply.github.com>
2023-01-14 12:36:58 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int arr[8], bt[8], visit[8], N, M;
+
+void back(int prev, int cnt){
+    int i;
+    if(cnt == M){
+        for(i = 0; i < M; i++)
+            printf("%d ", bt[i]);
+        puts("");
+    }
+    else{
+        for(i = prev; i < N; i++){
+            if(!visit[i] && bt[cnt] != arr[i]){
+                bt[cnt] = arr[i];
+                visit[i]++;
+                back(i + 1, cnt + 1);
+                visit[i]--;
+            }
+        }
+        bt[cnt] = 0;
+    }
+}
+
+int main(){
+    int i, j, temp;
+    scanf("%d %d", &N, &M);
+    for(i = 0; i < N; i++)
+        scanf("%d", &arr[i]);
+    for(i = 1; i <= N; i++){
+        for(j = 0; j < N - i; j++){
+            if(arr[j] > arr[j + 1]){
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    back(0, 0);
+
+    return 0;
+}
