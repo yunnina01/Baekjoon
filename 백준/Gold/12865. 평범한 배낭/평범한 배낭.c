be0bd86e421d1715130b6ed45df3019ae6f689ca
TaeHyun Yun <100751725+yunnina01@users.noreply.github.com>
2023-02-08 14:59:28 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 1508 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.c
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int dp[100001], W[100], V[100];
+int dp[100001], W[101], V[101];
 
 int max(int a, int b){
     return a > b ? a : b;
@@ -9,13 +9,11 @@ int max(int a, int b){
 int main(){
     int N, K, i, j;
     scanf("%d %d", &N, &K);
-    for(i = 0; i < N; i++)
+    for(i = 1; i <= N; i++)
         scanf("%d %d", &W[i], &V[i]);
     for(i = 1; i <= N; i++){
-        for(j = K; j > 0; j--){
-            if(W[i - 1] <= j)
-                dp[j] = max(dp[j], dp[j - W[i - 1]] + V[i - 1]);
-        }
+        for(j = K; j >= W[i]; j--)
+            dp[j] = max(dp[j], dp[j - W[i]] + V[i]);
     }
     printf("%d\n", dp[K]);
     
