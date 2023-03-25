adab40ec56576c6d68c60851a8b922bbe65d3c8f
TaeHyun Yun <100751725+yunnina01@users.noreply.github.com>
2023-03-25 13:40:03 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 24 ms, Memory: 1504 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int heap[100001], size, res;
+
+void push(int item){
+    int i = ++size;
+    while(i != 1 && item < heap[i / 2]){
+        heap[i] = heap[i / 2];
+        i /= 2;
+    }
+    heap[i] = item;
+}
+
+int pop(){
+    int parent = 1, child = 2, min = heap[1], temp = heap[size--];
+    while(child <= size){
+        if(child < size && heap[child] > heap[child + 1])
+            child++;
+        if(temp <= heap[child])
+            break;
+        heap[parent] = heap[child];
+        parent = child, child *= 2;
+    }
+    heap[parent] = temp;
+    return min;
+}
+
+int main(){
+    int N, n;
+    scanf("%d", &N);
+    while(N--){
+        scanf("%d", &n);
+        push(n);
+    }
+    while(size > 1){
+        n = pop() + pop();
+        res += n;
+        push(n);
+    }
+    printf("%d\n", res);
+
+    return 0;
+}
