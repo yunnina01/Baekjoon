bf189887c2ea2604eef99efcd97fe5bfbf50cb8b
TaeHyun Yun <100751725+yunnina01@users.noreply.github.com>
2023-03-29 19:40:50 +0900

    [Gold V] Title: 강의실, Time: 80 ms, Memory: 3072 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int s, e;
+}LEC;
+
+LEC lec[100000];
+int heap[100001], size;
+
+int asc(const void *a, const void *b){
+    return ((LEC*)a)->s - ((LEC*)b)->s;
+}
+
+void push(int item){
+    int i = ++size;
+    while(i != 1 && item < heap[i / 2]){
+        heap[i] = heap[i / 2];
+        i /= 2;
+    }
+    heap[i] = item;
+}
+
+void pop(){
+    int parent = 1, child = 2, temp = heap[size--];
+    while(child <= size){
+        if(child < size && heap[child] > heap[child + 1])
+            child++;
+        if(temp <= heap[child])
+            break;
+        heap[parent] = heap[child];
+        parent = child;
+        child *= 2;
+    }
+    heap[parent] = temp;
+}
+
+int main(){
+    int N, i, n;
+    scanf("%d", &N);
+    for(i = 0; i < N; i++)
+        scanf("%d %d %d", &n, &lec[i].s, &lec[i].e);
+    qsort(lec, N, sizeof(LEC), asc);
+    push(lec[0].e);
+    for(i = 1; i < N; i++){
+        if(lec[i].s >= heap[1])
+            pop();
+        push(lec[i].e);
+    }
+    printf("%d\n", size);
+
+    return 0;
+}
