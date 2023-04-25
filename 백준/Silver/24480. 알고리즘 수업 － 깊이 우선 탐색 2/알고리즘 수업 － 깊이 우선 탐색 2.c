f1baaaca144a95f09c6aafd65a413310c246ad57
TaeHyun Yun <100751725+yunnina01@users.noreply.github.com>
2023-04-25 18:31:35 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 132 ms, Memory: 8712 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *graph[100001];
+int visit[100001], size[100001], cnt;
+
+int desc(const void *a, const void *b){
+    return *(int*)b - *(int*)a;
+}
+
+void DFS(int R){
+    visit[R] = ++cnt;
+    for(int i = 0; i < size[R]; i++){
+        if(!visit[graph[R][i]])
+            DFS(graph[R][i]);
+    }
+}
+
+int main(){
+    int N, M, R, u, v, i;
+    scanf("%d %d %d", &N, &M, &R);
+    while(M--){
+        scanf("%d %d", &u, &v);
+        graph[u] = (int*)realloc(graph[u], sizeof(int) * (size[u] + 1));
+        graph[v] = (int*)realloc(graph[v], sizeof(int) * (size[v] + 1));
+        graph[u][size[u]++] = v;
+        graph[v][size[v]++] = u;
+    }
+    for(i = 1; i <= N; i++)
+        qsort(graph[i], size[i], sizeof(int), desc);
+    DFS(R);
+    for(i = 1; i <= N; i++)
+        printf("%d\n", visit[i]);
+
+    return 0;
+}
