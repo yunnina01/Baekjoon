4f461508b9aeba76a5d613eb679153f0693b55ba
TaeHyun Yun <100751725+yunnina01@users.noreply.github.com>
2023-04-26 19:22:32 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 112 ms, Memory: 7368 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *graph[100001];
+int queue[100000], visit[100001], size[100001], front, rear, cnt;
+
+int asc(const void *a, const void *b){
+    return *(int*)a - *(int*)b;
+}
+
+void BFS(int R){
+    int i, cur, next;
+    visit[R] = ++cnt;
+    queue[rear++] = R;
+    while(front != rear){
+        cur = queue[front++];
+        for(i = 0; i < size[cur]; i++){
+            next = graph[cur][i];
+            if(!visit[next]){
+                visit[next] = ++cnt;
+                queue[rear++] = next;
+            }
+        }
+    }
+}
+
+int main(){
+    int N, M, R, u, v, i;
+    scanf("%d %d %d", &N, &M, &R);
+    while(M--){
+        scanf("%d %d", &u, &v);
+        graph[u] = (int*)realloc(graph[u], sizeof(int) * (size[u] + 1));
+        graph[v] = (int*)realloc(graph[v], sizeof(int) * (size[v] + 1));
+        graph[u][size[u]++] = v;
+        graph[v][size[v]++] = u;
+    }
+    for(i = 1; i <= N; i++)
+        qsort(graph[i], size[i], sizeof(int), asc);
+    BFS(R);
+    for(i = 1; i <= N; i++)
+        printf("%d\n", visit[i]);
+
+    return 0;
+}
