3e19a723d3911bdf0ca8e04211f4d4b55b59e325
TaeHyun Yun <100751725+yunnina01@users.noreply.github.com>
2023-09-28 20:26:46 +0900

    [Platinum V] Title: 볼록 껍질, Time: 72 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Platinum/1708. 볼록 껍질/볼록 껍질.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ll long long
+
+typedef struct{
+    ll x, y;
+}Pair;
+
+Pair arr[100000], stack[100000];
+int top;
+
+ll ccw(Pair p1, Pair p2, Pair p3){
+    return p1.x * p2.y + p2.x * p3.y + p3.x * p1.y - (p2.x * p1.y + p3.x * p2.y + p1.x * p3.y);
+}
+
+int asc(const void *a, const void *b){
+    Pair *A = (Pair*)a, *B = (Pair*)b;
+    if(A->y == B->y)
+        return A->x - B->x;
+    return A->y - B->y;
+}
+
+ll orientation(Pair p1, Pair p2, Pair p3){
+    return (p2.y - p1.y) * (p3.x - p2.x) - (p2.x - p1.x) * (p3.y - p2.y);
+}
+
+ll get_dist(Pair p1, Pair p2){
+    return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+}
+
+int ccw_asc(const void *a, const void *b){
+    Pair *A = (Pair*)a, *B = (Pair*)b;
+    ll o = orientation(arr[0], *A, *B);
+    if(!o)
+        return get_dist(arr[0], *A) <= get_dist(arr[0], *B) ? -1 : 1;
+    if(o > 0)
+        return 1;
+    return -1;
+}
+
+int main(){
+    Pair p1, p2;
+    int N, i;
+    scanf("%d", &N);
+    for(i = 0; i < N; i++)
+        scanf("%lld %lld", &arr[i].x, &arr[i].y);
+    qsort(arr, N, sizeof(Pair), asc);
+    qsort(arr + 1, N - 1, sizeof(Pair), ccw_asc);
+    stack[top++] = arr[0];
+    stack[top++] = arr[1];
+    for(i = 2; i < N; i++){
+        while(top >= 2){
+            p2 = stack[--top];
+            p1 = stack[top - 1];
+            if(ccw(p1, p2, arr[i]) > 0){
+                stack[top++] = p2;
+                break;
+            }
+        }
+        stack[top++] = arr[i];
+    }
+    printf("%d\n", top);
+
+    return 0;
+}
