193ae195527e1cbc0121f72f499e8c11022a784c
TaeHyun Yun <100751725+yunnina01@users.noreply.github.com>
2023-10-12 19:48:46 +0900

    [Gold V] Title: 알약, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/4811. 알약/알약.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+dp = [[0] * 31 for _ in range(31)]
+for i in range(1, 31):
+    dp[0][i] = 1
+for i in range(1, 31):
+    for j in range(i, 31):
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+while 1:
+    N = int(input())
+    if N == 0:
+        break
+    print(dp[N][N])
