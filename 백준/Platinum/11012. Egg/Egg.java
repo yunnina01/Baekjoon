6718c5732eb0e076e9cd0f939e703f71e64690a1
TaeHyun Yun <100751725+yunnina01@users.noreply.github.com>
2024-01-08 21:24:16 +0900

    [Platinum II] Title: Egg, Time: 2800 ms, Memory: 319504 KB -BaekjoonHub

--- 백준/Platinum/11012. Egg/Egg.java
+++ 백준/Platinum/11012. Egg/Egg.java
@@ -2,133 +2,103 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int RANGE = 100000;
-	static Node[] parades;
+    static final int RANGE = 100001;
+    static long[] tree, lazy;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer  st;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-		int T = Integer.parseInt(br.readLine());
-		while(T-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(br.readLine());
+        while(T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
 
-			int[][] points = new int[n + 1][2];
-			for(int i = 1; i <= n; i++) {
-				st = new StringTokenizer(br.readLine());
-				points[i][0] = Integer.parseInt(st.nextToken());
-				points[i][1] = Integer.parseInt(st.nextToken());
-			}
-			Arrays.sort(points, new Comparator<int[]>() {
-				@Override
-				public int compare(int[] o1, int[] o2) {
-					if(o1[0] == o2[0])
-						return Integer.compare(o1[1], o2[1]);
-					return Integer.compare(o1[0], o2[0]);
-				}
-			});
+            List<int[]> paradeList = new ArrayList<>();
+            for(int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                paradeList.add(new int[]{x, y, 0, 1});
+            }
 
-			Map<Integer, Integer> map = new HashMap<>();
-			for(int i = 1; i <= n; i++)
-				map.put(points[i][0], i);
+            for(int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+                int l = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                int t = Integer.parseInt(st.nextToken());
 
-			parades = new Node[n + 1];
-			parades[0] = new Node(0, null, null);
-            parades[0].left = parades[0];
-            parades[0].right = parades[0];
-			for(int i = 1; i <= n; i++)
-				parades[i] = update(parades[i - 1], 0, RANGE, points[i][1]);
+                paradeList.add(new int[]{l, b, t, 0});
+                paradeList.add(new int[]{r, b, t, 2});
+            }
 
-			int res = 0;
-			for(int i = 0; i < m; i++) {
-				st = new StringTokenizer(br.readLine());
-				int l = Integer.parseInt(st.nextToken());
-				int r = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				int t = Integer.parseInt(st.nextToken());
+            tree = new long[RANGE * 4];
+            lazy = new long[RANGE * 4];
+            paradeList.sort((o1, o2) -> {
+                if(o1[0] != o2[0])
+                    return o1[0] - o2[0];
+                return o1[3] - o2[3];
+            });
 
-				int lb = lowerBound(points, l, r);
-				int ub = upperBound(points, l, r);
-				if(lb == -1 || ub == -1)
-					continue;
-
-				if(ub >= 1)
-					res += query(parades[map.get(points[ub][0])], 0, RANGE, b, t);
-				if(lb >= 2)
-					res -= query(parades[map.get(points[lb - 1][0])], 0, RANGE, b, t);
-			}
-			bw.write(res + "\n");
-		}
+            long res = 0;
+            for(int[] parade : paradeList) {
+                if(parade[3] == 1) {
+                    if(tree[1] == 0)
+                        continue;
+                    res += query(1, 0, RANGE - 1, parade[1]);
+                } else {
+                    if(parade[3] == 0)
+                        update(1, 0, RANGE - 1, parade[1], parade[2], 1);
+                    else
+                        update(1, 0, RANGE - 1, parade[1], parade[2], -1);
+                }
+            }
+            bw.write(res + "\n");
+        }
         br.close();
         bw.flush();
-		bw.close();
-	}
+        bw.close();
+    }
 
-	static int lowerBound(int[][] points, int left, int right) {
-		int low = 1;
-		int high = points.length - 1;
+    static void updateLazy(int node, int start, int end) {
+        if(lazy[node] != 0) {
+            tree[node] += (end - start + 1) * lazy[node];
+            if (start != end) {
+                lazy[node * 2] += lazy[node];
+                lazy[node * 2 + 1] += lazy[node];
+            }
+            lazy[node] = 0;
+        }
+    }
 
-		while(low < high) {
-			int mid = (low + high) >> 1;
-			if(points[mid][0] < left)
-				low = mid + 1;
-			else
-				high = mid;
-		}
-		if(points[low][0] < left)
-			return -1;
-		return low;
-	}
+    static void update(int node, int start, int end, int left, int right, int val) {
+        updateLazy(node, start, end);
+        if(start > right || end < left)
+            return;
+        if (left <= start && end <= right) {
+            tree[node] += (end - start + 1) * val;
+            if(start != end) {
+                lazy[node * 2] += val;
+                lazy[node * 2 + 1] += val;
+            }
+            return;
+        }
+        int mid = (start + end) >> 1;
+        update(node * 2, start, mid, left, right, val);
+        update(node * 2 + 1, mid + 1, end, left, right, val);
+        tree[node] = tree[node * 2] + tree[node * 2 + 1];
+    }
 
-	static int upperBound(int[][] points, int left, int right) {
-		int low = 1;
-		int high = points.length - 1;
-
-		while(low < high) {
-			int mid = (low + high) >> 1;
-			if(points[mid][0] <= right)
-				low = mid + 1;
-			else
-				high = mid;
-		}
-		if(points[low][0] <= right)
-			return low;
-		if(low - 1 >= 0 && points[low - 1][0] <= right)
-			return low - 1;
-		return -1;
-	}
-
-	static Node update(Node p, int start, int end, int idx) {
-		if(idx < start || idx > end)
-			return p;
-		if(start == end)
-			return new Node(p.v + 1, null, null);
-
-		int mid = (start + end) >> 1;
-		return new Node(p.v + 1, update(p.left, start, mid, idx), update(p.right, mid + 1, end, idx));
-	}
-
-	static int query(Node p, int start, int end, int left, int right) {
-		if(start > right || end < left)
-			return 0;
-		if(start >= left && end <= right)
-			return p.v;
-
-		int mid = (start + end) >> 1;
-		return query(p.left, start, mid, left, right) + query(p.right, mid + 1, end, left, right);
-	}
-}
-
-class Node {
-	int v;
-	Node left, right;
-
-	Node(int v, Node left, Node right) {
-		this.v = v;
-		this.left = left;
-		this.right = right;
-	}
+    static long query(int node, int start, int end, int idx) {
+        updateLazy(node, start, end);
+        if(idx < start || idx > end)
+            return 0;
+        if(start == end)
+            return tree[node];
+        int mid = (start + end) >> 1;
+        return query(node * 2, start, mid, idx) + query(node * 2 + 1, mid + 1, end, idx);
+    }
 }
