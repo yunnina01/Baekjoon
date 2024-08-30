3ccafdd132b6651b0c69bcd66a465c602410f3b1
TaeHyun Yun <yunnina01@naver.com>
2024-08-30 21:20:41 +0900

    [Gold II] Title: 줄 세우기, Time: 416 ms, Memory: 155528 KB -BaekjoonHub

--- 백준/Gold/7570. 줄 세우기/줄 세우기.py
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.py
@@ -2,16 +2,9 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-child = list(map(int, input().split()))
-dp = [-1] * (N + 1)
-for i, v in enumerate(child):
-    dp[v] = i
-longest, cnt = 0, 1
+numbers = list(map(int, input().split()))
 
-for i in range(1, N):
-    if dp[i] < dp[i + 1]:
-        cnt += 1
-    else:
-        longest = max(longest, cnt)
-        cnt = 1
-print(N - longest if N != 1 else 0)
+res = [0] * (N + 1)
+for num in numbers:
+    res[num] = res[num - 1] + 1
+print(N - max(res))
