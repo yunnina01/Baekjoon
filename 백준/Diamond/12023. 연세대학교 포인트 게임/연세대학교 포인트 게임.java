d329d388bbb7236f3a24542afaa6ab7a9c5aad2b
TaeHyun Yun <yunnina01@naver.com>
2025-04-20 09:18:14 +0900

    [Diamond IV] Title: 연세대학교 포인트 게임, Time: 1180 ms, Memory: 153724 KB -BaekjoonHub

---
+++ 백준/Diamond/12023. 연세대학교 포인트 게임/연세대학교 포인트 게임.java
@@ -0,0 +1,188 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static Node[] adj;
+	static int[][] dp;
+	static long[] distance;
+	static int[] ct, depth, subSize;
+	static boolean[] blue, visit;
+	static Result[] results;
+	static int N, logN;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+
+		adj = new Node[N + 1];
+		for(int i = 1; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			long B = Long.parseLong(st.nextToken());
+
+			adj[i] = new Node(A, B, adj[i]);
+			adj[A] = new Node(i, B, adj[A]);
+		}
+
+		logN = (int)Math.ceil(Math.log(N) / Math.log(2.0));
+		dp = new int[logN + 1][N + 1];
+		distance = new long[N + 1];
+		depth = new int[N + 1];
+		DFS(0, N, 0L);
+
+		for(int i = 1; i <= logN; i++) {
+			for(int j = 0; j < N; j++)
+				dp[i][j] = dp[i - 1][dp[i - 1][j]];
+		}
+
+		ct = new int[N];
+		subSize = new int[N];
+		visit = new boolean[N];
+		buildCentroidTree(0, N);
+
+		blue = new boolean[N];
+		results = new Result[N];
+		for(int i = 0; i < N; i++)
+			results[i] = new Result();
+		
+		int Q = Integer.parseInt(br.readLine());
+
+		for(int i = 0; i < Q; i++) {
+			st = new StringTokenizer(br.readLine());
+			if(st.nextToken().charAt(0) == '1')
+				update(Integer.parseInt(st.nextToken()));
+			else
+				sb.append(query(Integer.parseInt(st.nextToken())) + "\n");
+		}
+        bw.write(sb.toString());
+        bw.flush();
+	}
+	
+	static void DFS(int now, int parent, long weight) {
+		dp[0][now] = parent;
+		depth[now] = depth[parent] + 1;
+		distance[now] = distance[parent] + weight;
+		for(Node child = adj[now]; child != null; child = child.next) {
+			if(child.idx == parent)
+				continue;
+			DFS(child.idx, now, child.weight);
+		}
+	}
+	
+	static void buildCentroidTree(int now, int parent) {
+		int centroid = getCentroid(now, -1, getSize(now, -1) >> 1);
+		ct[centroid] = parent;
+		visit[centroid] = true;
+		for(Node child = adj[centroid]; child != null; child = child.next) {
+			if(visit[child.idx])
+				continue;
+			buildCentroidTree(child.idx, centroid);
+		}
+	}
+	
+	static int getCentroid(int now, int parent, int threshold) {
+		for(Node child = adj[now]; child != null; child = child.next) {
+			if(visit[child.idx] || child.idx == parent)
+				continue;
+			if(subSize[child.idx] > threshold)
+				return getCentroid(child.idx, now, threshold);
+		}
+		return now;
+	}
+	
+	static int getSize(int now, int parent) {
+		subSize[now] = 1;
+		for(Node child = adj[now]; child != null; child = child.next) {
+			if(visit[child.idx] || child.idx == parent)
+				continue;
+			subSize[now] += getSize(child.idx, now);
+		}
+		return subSize[now];
+	}
+	
+	static void update(int v) {
+		if(blue[v])
+			return;
+
+        int i, prev;
+		blue[v] = true;
+		results[v].add(v, 0L);
+		for(prev = v, i = ct[v]; i != N; prev = i, i = ct[i])
+			results[i].add(prev, getDist(i, v));
+	}
+	
+	static long getDist(int u, int v) {
+		return distance[u] + distance[v] - (distance[LCA(u, v)] << 1);
+	}
+	
+	static int LCA(int u, int v) {
+		if(depth[u] < depth[v]) {
+			int temp = u;
+			u = v;
+			v = temp;
+		}
+		for(int diff = depth[u] - depth[v], i = 0; diff != 0; diff >>= 1, i++) {
+			if((diff & 1) == 1)
+				u = dp[i][u];
+		}
+		if(u == v)
+			return u;
+
+		for(int i = logN; i >= 0; i--) {
+			if(dp[i][u] != dp[i][v]) {
+				u = dp[i][u];
+				v = dp[i][v];
+			}
+		}
+		return dp[0][u];
+	}
+	
+	static long query(int v) {
+		int i, prev;
+		long ret = 0L;
+		for(prev = N, i = v; i != N; prev = i, i = ct[i]) {
+			long dist = getDist(i, v);
+			ret += results[i].calc(prev, dist);
+		}
+		return ret;
+	}
+}
+	
+class Node {
+    int idx;
+    long weight;
+    Node next;
+    
+    Node(int idx, long weight, Node next) {
+        this.idx = idx;
+        this.weight = weight;
+        this.next = next;
+    }
+}
+
+class Result {
+    int cnt;
+    long sum;
+    HashMap<Integer, Integer> cnts;
+    HashMap<Integer, Long> sums;
+    
+    Result() {
+        cnts = new HashMap<>();
+        sums = new HashMap<>();
+    }
+    
+    void add(int idx, long dist) {
+        cnt++;
+        sum += dist;
+        cnts.put(idx, cnts.getOrDefault(idx, 0) + 1);
+        sums.put(idx, sums.getOrDefault(idx, 0L) + dist);
+    }
+    
+    long calc(int idx, long dist) {
+        return (cnt - cnts.getOrDefault(idx, 0)) * dist + sum - sums.getOrDefault(idx, 0L);
+    }
+}
