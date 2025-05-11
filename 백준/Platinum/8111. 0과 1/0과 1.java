f880a093f36252766144850d309527436417f7d5
TaeHyun Yun <yunnina01@naver.com>
2025-05-12 06:04:42 +0900

    [Platinum V] Title: 0과 1, Time: 180 ms, Memory: 18480 KB -BaekjoonHub

---
+++ 백준/Platinum/8111. 0과 1/0과 1.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static boolean[] visit;
+	static int[] dr = {0, 1};
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		while(T-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+			
+            visit = new boolean[N];
+			sb.append(BFS(N) + "\n");
+		}
+		bw.write(sb.toString());
+        bw.flush();
+	}
+
+    static String BFS(int mod) {
+	    LinkedList<Node> queue = new LinkedList<>();
+		queue.offer(new Node(1 % mod, "1"));
+		visit[1 % mod] = true;
+
+		String res = "BRAK";
+		while(!queue.isEmpty()) {
+			Node now = queue.poll();
+			if(now.r == 0) {
+				res = now.s;
+				break;
+			} else if(now.s.length() >= 100)
+				break;
+
+			for(int i = 0; i < 2; i++) {
+				int nr = (now.r * 10 + dr[i]) % mod;
+				if(visit[nr])
+					continue;
+				visit[nr] = true;
+				queue.offer(new Node(nr, now.s + dr[i]));
+			}
+		}
+		return res;
+	}
+}
+
+class Node {
+    int r;
+    String s;
+
+    Node(int r, String s) {
+        this.r = r;
+        this.s = s;
+    }
+}
